4856ce02017bd61b59644bb351f39471ff5c7276
SandraVLP <aleksanvp@yandex.ru>
2024-06-10 20:51:01 +0300

    navigation styles refactoring

--- src/layout/sections/Navigation/Navigation.tsx
+++ src/layout/sections/Navigation/Navigation.tsx
@@ -1,41 +1,20 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import styled from 'styled-components'
+import { S } from './Navigation_styles'
 
-export function Navigation() {
+export const Navigation = () => {
     return (
         <nav className='nav' style={{flexShrink: '0', maxWidth: '200px', width: '100%'}}>
-            <StyledUl>
-                <StyledLi>
-                    <StyledLink to='/profile'>Profile</StyledLink>
-                </StyledLi>
-                <StyledLi><StyledLink to='/dialogs'>Messages</StyledLink> </StyledLi>
-                <StyledLi><StyledLink to='/news'>News</StyledLink> </StyledLi>
-                <StyledLi><StyledLink to='/music'>Music</StyledLink> </StyledLi>
-                <StyledLi><StyledLink to='/settings'>Settings</StyledLink> </StyledLi>
-            </StyledUl>
+            <S.StyledUl>
+                <S.StyledLi>
+                    <S.StyledLink to='/profile'>Profile</S.StyledLink>
+                </S.StyledLi>
+                <S.StyledLi><S.StyledLink to='/dialogs'>Messages</S.StyledLink> </S.StyledLi>
+                <S.StyledLi><S.StyledLink to='/news'>News</S.StyledLink> </S.StyledLi>
+                <S.StyledLi><S.StyledLink to='/music'>Music</S.StyledLink> </S.StyledLi>
+                <S.StyledLi><S.StyledLink to='/settings'>Settings</S.StyledLink> </S.StyledLi>
+            </S.StyledUl>
         </nav>
     )
 }
-
-const StyledUl = styled.ul`
-    list-style-type: none;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    background-color: white;
-`
-
-const StyledLi = styled.li`
-    cursor: pointer;
-    text-decoration: none;
-`
-const StyledLink = styled(NavLink)`
-    color: black;
-    cursor: pointer;
-    text-decoration: none;
-
-    &.active {
-        color: rgb(4, 149, 159);
-    }
-`
