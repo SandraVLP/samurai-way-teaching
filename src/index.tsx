f1afb0845c71fa18af2013363f4175018d1d2eb5
SandraVLP <aleksanvp@yandex.ru>
2024-06-13 09:37:19 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,12 @@ import {ThemeProvider} from "styled-components";
 import {GlobalStyles} from "./styles/GlobalStyles";
 import {Theme} from "./styles/Theme";
 import {BrowserRouter} from "react-router-dom";
+import AppStart from './AppStart';
 
 ReactDOM.render(
     <ThemeProvider theme={Theme}>
         <BrowserRouter>
-            <App/>
+            <AppStart/>
             <GlobalStyles/>
         </BrowserRouter>
     </ThemeProvider>,
